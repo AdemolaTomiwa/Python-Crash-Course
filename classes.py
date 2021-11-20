2d0151de52e4bef1cffed9de7537e6d9e3804572
thetommedia@gmail.com <thetommedia@gmail.com>
2021-11-20 19:25:24 +0100

    Final commit

--- classes.py
+++ classes.py
@@ -1,2 +1,43 @@
 # A class is like a blueprint for creating objects. An object has properties and methods(functions) associated with it. Almost everything in Python is an object
 
+# Create a class
+class User:
+    def __init__(self, name, email, age):
+        self.name = name
+        self.email = email
+        self.age = age
+
+    def greeting(self):
+        return f'My name is {self.name} and I am {self.age}'
+
+    def hasBirthday(self):
+        self.age += 1
+
+# Extend class
+class Customer(User):
+    def __init__(self, name, email, age):
+        self.name = name
+        self.email = email
+        self.age = age
+        self.balance = 0
+
+    def set_balance(self, balance):
+        self.balance = balance
+
+    def greeting(self):
+        return f'My name is {self.name} and I am {self.age} and my balance is {self.balance}'
+
+# Init User Object
+brad = User('Brad Traversy', 'brad@gmail.com', 37)
+
+# Init Customer object
+janet = Customer('Janet Smith', 'janet@gmail.com', 25)
+
+janet.set_balance(500)
+print(janet.greeting())
+
+brad.hasBirthday()
+
+# print(brad)
+# print(brad.name)
+print(brad.greeting())
--- files.py
+++ files.py
@@ -1,1 +1,23 @@
 # Python has functions for creating, reading, updating, and deleting files.
+
+# Open a file
+myFile = open('myFile.txt', 'w')
+
+# Get info
+print('Name: ', myFile.name)
+print('Is Closed: ', myFile.closed)
+print('Opening Mode: ', myFile.mode)
+
+# Write to file
+myFile.write('I Love Python')
+myFile.write(' and Javascript')
+myFile.close()
+
+# Append to file
+myFile = open('myFile.txt', 'a')
+myFile.write(', I also like SASS')
+
+# Read from a file
+myFile = open('myFile.txt', 'r+')
+text = myFile.read(100)
+print(text)
